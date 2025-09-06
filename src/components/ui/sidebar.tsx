3f292e33007c87697433751def8212f7f37ebcc4
Martin Karugaba <martinkarugaba21@gmail.com>
2025-09-06 10:17:02 +0300

    Increase the sidebar width

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -139,7 +139,7 @@ function SidebarProvider({
             } as React.CSSProperties
           }
           className={cn(
-            "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full border-2",
+            "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full",
             className
           )}
           {...props}
