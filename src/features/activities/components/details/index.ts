3c348bd786c9c229891ff21e342f32c2261f41e1
Martin Karugaba <martinkarugaba21@gmail.com>
2025-10-11 13:26:22 +0300

    feat: add AttendanceOverviewTab and SessionsManagementTab components for enhanced attendance analytics and session management

--- src/features/activities/components/details/index.ts
+++ src/features/activities/components/details/index.ts
@@ -3,3 +3,5 @@ export { ActivityDetailsTabs } from "./activity-details-tabs";
 export { AttendanceAnalyticsTab } from "./attendance-analytics-tab";
 export { AttendanceDemographicsTab } from "./attendance-demographics-tab";
 export { AttendanceTab } from "./attendance-tab";
+export { SessionsManagementTab } from "./sessions-management-tab";
+export { AttendanceOverviewTab } from "./attendance-overview-tab";
